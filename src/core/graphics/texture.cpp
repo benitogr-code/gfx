5e6e0778bbc1a22194a3d120c659e5b726cc98b3
Benito GR <bgr.online@posteo.net>
2022-01-25 18:08:47 +0100

    feat: cubemap textures

--- src/core/graphics/texture.cpp
+++ src/core/graphics/texture.cpp
@@ -3,6 +3,16 @@
 
 #include <glad/glad.h>
 
+// Helpers
+static GLint MapWrapMode(TextureWrapMode mode) {
+  switch(mode) {
+    case TextureWrapMode::Repeat: return GL_REPEAT;
+    case TextureWrapMode::Mirror: return GL_MIRRORED_REPEAT;
+    case TextureWrapMode::ClampToEdge: return GL_CLAMP_TO_EDGE;
+    case TextureWrapMode::ClampToBorder: return GL_CLAMP_TO_BORDER;
+  }
+}
+
 Texture::Texture()
   : _id(0) {
 }
@@ -15,13 +25,13 @@ void Texture::bind() {
   glBindTexture(GL_TEXTURE_2D, _id);
 }
 
-void Texture::loadImage(const ImageData& image) {
+void Texture::load2DImage(const ImageData& image, TextureWrapMode wrapMode) {
   glGenTextures(1, &_id);
   glBindTexture(GL_TEXTURE_2D, _id);
 
   // Wrapping params
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, MapWrapMode(wrapMode)); // set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, MapWrapMode(wrapMode));
   // Filtering params
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -31,13 +41,61 @@ void Texture::loadImage(const ImageData& image) {
   glGenerateMipmap(GL_TEXTURE_2D);
 }
 
+void Texture::load3DImage(const std::vector<ImageData>& images) {
+  glGenTextures(1, &_id);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, _id);
+
+  // Wrapping params
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+  // Filtering params
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  for (int i = 0; i < images.size(); ++i) {
+    auto& image = images[i];
+
+    const GLenum format = image.bytesPerPixel == 4 ? GL_RGBA : GL_RGB;
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, image.width, image.height, 0, format, GL_UNSIGNED_BYTE, image.data.data());
+  }
+}
+
+
 /*static*/ TextureRef Texture::Create(const TextureCreateParams& params) {
   TextureRef texture(new Texture());
 
   ImageData image;
   if (FileUtils::readPngFile(params.filePath, image)) {
-    texture->loadImage(image);
+    texture->load2DImage(image, params.wrapmode);
   }
+
+  return texture;
+}
+
+/*static*/ TextureRef Texture::CreateCubemap(const Texture3DCreateParams& params) {
+  TextureRef texture(new Texture());
+
+  const int kRequiredTextures = 6;
+  const int fileCount = std::min((int)params.filePaths.size(), kRequiredTextures);
+
+  std::vector<ImageData> images;
+  images.resize(fileCount);
+  int texturesLoaded = 0;
+  for (int i = 0; i < fileCount; ++i) {
+    if (FileUtils::readPngFile(params.filePaths[i].c_str(), images[i])) {
+      texturesLoaded++;
+    }
+  }
+
+  if (texturesLoaded == kRequiredTextures) {
+    texture->load3DImage(images);
+  }
+  else {
+    LOG_WARN("[Texture] Cube map requires {} textures but only got {}", kRequiredTextures, texturesLoaded);
+  }
+
 
   return texture;
 }
--- src/core/graphics/texture.h
+++ src/core/graphics/texture.h
@@ -5,12 +5,37 @@ struct ImageData;
 class Texture;
 typedef std::shared_ptr<Texture> TextureRef;
 
+enum class TextureWrapMode {
+  Repeat = 0,
+  Mirror,
+  ClampToEdge,
+  ClampToBorder
+};
+
 struct TextureCreateParams {
   TextureCreateParams()
-    : filePath(nullptr) {
+    : filePath(nullptr)
+    , wrapmode(TextureWrapMode::Repeat) {
     }
 
-  const char* filePath;
+  const char*     filePath;
+  TextureWrapMode wrapmode;
+};
+
+struct Texture3DCreateParams {
+  enum Faces {
+    Right = 0,
+    Left,
+    Top,
+    Bottom,
+    Back,
+    Front,
+    Count
+  };
+
+  Texture3DCreateParams() {}
+
+  std::array<std::string, Faces::Count> filePaths;
 };
 
 class Texture {
@@ -21,12 +46,14 @@ public:
   void bind();
 
   static TextureRef Create(const TextureCreateParams& params);
+  static TextureRef CreateCubemap(const Texture3DCreateParams& params);
 
 private:
   Texture();
   Texture(const Texture& texture) = delete;
 
-  void loadImage(const ImageData& image);
+  void load2DImage(const ImageData& image, TextureWrapMode wrapMode);
+  void load3DImage(const std::vector<ImageData>& images);
 
 private:
   unsigned int _id;
